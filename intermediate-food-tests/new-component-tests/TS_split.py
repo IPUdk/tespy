d5c214cf73798dbdd03d099d00e5ab0317c4b6f0
Martin Ryhl Kærn <mrk@ipu.dk>
2023-01-17 12:05:16 +0100

    changed some naming and cleaned the code..

--- intermediate-food-tests/new-component-tests/TS_split.py
+++ intermediate-food-tests/new-component-tests/TS_split.py
@@ -177,28 +177,25 @@ class SeparatorWithCompositionSplits(Separator):
 
     @staticmethod
     def component():
-        return 'separator with composition splits'
+        return 'separator with species flow splits'
 
     def get_variables(self):
         variables = super().get_variables()
-        variables["TS"] = self.dc_cp_split(
+        variables["SFS"] = self.dc_cp_SFS(
             min_val=0,
-            deriv=self.TS_deriv,
-            func=self.TS_func,
+            deriv=self.SFS_deriv,
+            func=self.SFS_func,
             latex=self.pr_func_doc,
             num_eq=1,
-            split_fluid = 'Water',
-            split_no = 0,
         )
         return variables
 
-    class dc_cp_split(ComponentProperties):
+    class dc_cp_SFS(ComponentProperties):
         """
         Data container for component properties. 
-        + split
-
+        + SFS_fluid
+        + SFS_outlet
         """
-
         @staticmethod
         def attr():
             """
@@ -215,26 +212,9 @@ class SeparatorWithCompositionSplits(Separator):
                 'min_val': -1e12, 'max_val': 1e12, 'is_var': False,
                 'val_ref': 1, 'design': np.nan, 'is_result': False,
                 'num_eq': 0, 'func_params': {}, 'func': None, 'deriv': None,
-                'latex': None, 'split_fluid' : str, 'split_no' : int}
+                'latex': None, 'split_fluid' : str, 'split_outlet' : str}
 
-    # def get_mandatory_constraints(self):
-    #     return {
-    #         'mass_flow_constraints': {
-    #             'func': self.mass_flow_func, 'deriv': self.mass_flow_deriv,
-    #             'constant_deriv': True, 'latex': self.mass_flow_func_doc,
-    #             'num_eq': 1},
-    #         'fluid_constraints': {
-    #             'func': self.fluid_func, 'deriv': self.fluid_deriv,
-    #             'constant_deriv': False, 'latex': self.fluid_func_doc,
-    #             'num_eq': self.num_nw_fluids},
-    #         'energy_balance_constraints': {
-    #             'func': self.energy_balance_func,
-    #             'deriv': self.energy_balance_deriv,
-    #             'constant_deriv': False, 'latex': self.energy_balance_func_doc,
-    #             'num_eq': 1}
-    #     }
-
-    def TS_func(self):
+    def SFS_func(self):
         r"""
         Equation for pressure drop.
 
@@ -255,16 +235,16 @@ class SeparatorWithCompositionSplits(Separator):
         #     residual += [res]
         # return residual
 
-        fluid = self.TS.split_fluid
-        out_i = self.TS.split_no
+        fluid = self.SFS.split_fluid
+        out_i = int(self.SFS.split_outlet[3:]) - 1
 
-        res = self.inl[0].fluid.val[fluid] * self.inl[0].m.val_SI * self.TS.val \
+        res = self.inl[0].fluid.val[fluid] * self.inl[0].m.val_SI * self.SFS.val \
             - self.outl[out_i].fluid.val[fluid] * self.outl[out_i].m.val_SI 
 
         #print(res)
         return res
 
-    def TS_deriv(self, increment_filter, k):
+    def SFS_deriv(self, increment_filter, k):
         r"""
         Calculate the partial derivatives for combustion pressure ratio.
 
@@ -293,33 +273,21 @@ class SeparatorWithCompositionSplits(Separator):
         #     i += 1
         #     k += 1
 
-        fluid_index = list(self.inl[0].fluid.val.keys()).index(self.TS.split_fluid)
-        fluid = self.TS.split_fluid
-        out_i = self.TS.split_no
+        fluid_index = list(self.inl[0].fluid.val.keys()).index(self.SFS.split_fluid)
+        fluid = self.SFS.split_fluid
+        out_i = int(self.SFS.split_outlet[3:]) - 1
 
         i = fluid_index
         j = 0 
-        self.jacobian[k, j, 0]     = self.inl[0].fluid.val[fluid] * self.TS.val 
-        self.jacobian[k, j, i + 3] = self.inl[0].m.val_SI * self.TS.val 
+        self.jacobian[k, j, 0]     = self.inl[0].fluid.val[fluid] * self.SFS.val 
+        self.jacobian[k, j, i + 3] = self.inl[0].m.val_SI * self.SFS.val 
         j = 1 + out_i 
         self.jacobian[k, j, 0]     = -self.outl[out_i].fluid.val[fluid]
         self.jacobian[k, j, i + 3] = -self.outl[out_i].m.val_SI 
         
-        
         #print(self.jacobian)
         #print(self.jacobian[k,:,:])
-        
-    def calc_parameters(self):
 
-        print('hey')
-        # self.pr.val = self.outl[0].p.val_SI / self.inl[0].p.val_SI
-        # for i in range(self.num_i):
-        #     if self.inl[i].p.val < self.outl[0].p.val:
-        #         msg = (
-        #             f"The pressure at inlet {i + 1} is lower than the pressure "
-        #             f"at the outlet of component {self.label}."
-        #         )
-        #         logging.warning(msg)
 
 
 
@@ -330,8 +298,6 @@ fluids = ["INCOMP::Water", "INCOMP::T66"]
 nw = Network(fluids=fluids, p_unit="bar", T_unit="C")
 
 so = Source("Source")
-#  Variant 2: Q is m (h_2 - h_1), Q_total is taking efficiency into account and represents the heat transfer over system
-# boundary. For heat transfer into the system: Q = Q_total * eta, for heat transfer from the system: Q_total = Q * eta
 se = SeparatorWithCompositionSplits("Separator")
 si1 = Sink("Sink 1")
 si2 = Sink("Sink 2")
@@ -345,15 +311,27 @@ nw.add_conns(c1, c2, c3)
 # set some generic data for starting values
 c1.set_attr(m=1, p=1.2, T=30, fluid={"Water": 0.9, "T66": 0.1})
 
+# set compositions 
 c2.set_attr(fluid={"Water": 0.85, "T66": 0.15})
-# c2.set_attr(m=0.5)
-se.set_attr(TS={
-    'val': 0.6, 'is_set': True,   
-    'split_fluid' : 'T66', 'split_no' : 0})
+# or others 
+#c3.set_attr(fluid={"Water": 0.85, "T66": 0.15})
+# or others
+#c2.set_attr(fluid={"Water": 0.85})
+#c3.set_attr(fluid={"Water": 1})
 
+# This one produce error because T66 is 0 and the equation cannot be solved.. 
+#c2.set_attr(fluid={"Water": 1, "T66": 0.0})
 
-c2.set_attr(m0=0.5,h0=1e5,T0=50,fluid0={"Water": 0.5, "T66": 0.5})
-c3.set_attr(m0=0.5,h0=1e5,T0=50,fluid0={"Water": 0.5, "T66": 0.5})
+# specify this one to avoid using the species flow split : SFS  
+#c2.set_attr(m=0.5)
+# set the species flow split, specify the fluid and the outlet too.. (we might need some checks of this)
+se.set_attr(SFS={
+    'val': 0.6, 'is_set': True, 
+    'split_fluid' : 'T66', 'split_outlet' : "out1"})
+
+# add some guess values
+c2.set_attr(m0=0.5,p0=1.2,h0=1e5,T0=50,fluid0={"Water": 0.5, "T66": 0.5})
+c3.set_attr(m0=0.5,p0=1.2,h0=1e5,T0=50,fluid0={"Water": 0.5, "T66": 0.5})
 
 nw.solve("design")
 nw.print_results()
@@ -362,15 +340,5 @@ print(nw.results['Connection'])
 
 m_T66_c1 = c1.m.val * c1.fluid.val['T66']
 m_T66_c2 = c2.m.val * c2.fluid.val['T66']
-print(f"TS split is {m_T66_c2/m_T66_c1}")
+print(f"Species flow split is {m_T66_c2/m_T66_c1}")
 
-
-
-
-
-# he.Q.val
-# he.Q_loss.val
-# he.Q_total.val
-
-
-# %%
